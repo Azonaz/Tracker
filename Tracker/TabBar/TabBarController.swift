56ee362286ac057e8943ae4f3fe60e7db451c692
Azonaz <malinina.a@gmail.com>
2023-09-20 15:45:01 +0300

    подправлен внешний вид табБара согласно макету

--- Tracker/TabBar/TabBarController.swift
+++ Tracker/TabBar/TabBarController.swift
@@ -1,18 +1,20 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addTabBarController()
     }
-
+    
     func addTabBarController() {
         tabBar.backgroundColor = .ypWhite
         tabBar.barTintColor = .ypBlue
-        let borderLine = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.size.width, height: 1))
-        borderLine.backgroundColor = .ypLightGray
-        tabBar.addSubview(borderLine)
+        if UITraitCollection.current.userInterfaceStyle == .light {
+            let borderLine = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.size.width, height: 1))
+            borderLine.backgroundColor = .ypLightGray
+            tabBar.addSubview(borderLine)
+        }
         let trackerViewController = TrackersViewController()
         let trackerNavigationController = UINavigationController(rootViewController: trackerViewController)
         trackerNavigationController.tabBarItem = UITabBarItem(title: trackersTabTitile,
